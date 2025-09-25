8d613b4b19caeabf23f9cb49f07609abecd00836
Villamayor <villamayorgabriel12@example.com>
2025-09-26 00:59:20 +0200

    P4 fonctionnel

--- P4.py
+++ P4.py
@@ -24,7 +24,6 @@ def afficher_grille():
         print(ligne_affichee.strip())
     print("-" * 29)
 
-afficher_grille()
 
 # jouer 1 = jeton 1 et joueur 2 = jeton 2 
 
@@ -35,8 +34,34 @@ def changer_joueur():
     else:
         joueur_courant = 1
 
+def jouer_colonne(col):
+    for i in range(len(tableau)-1, -1, -1):
+        if tableau[i][col] == 0:
+            tableau[i][col] = joueur_courant
+            return True
+    return False  
+
+# Sortie principale
+
 # demander au joueur 1 de choisir la case de la premiere range puis de meme au joueur 2 etc...
 
-# si un des joeur rempli les regles pour gagner (4 jetons alignes horizontalement, verticalement ou den diagonale) il est declare gagnant
+print("Coucou l'ami, joue au Puissance 4 !")
 
-input("Appuyez sur Entrée pour fermer...")
+while True:
+    try:
+        choix = int(input(f"Joueur {joueur_courant}, quelle colonne choisis-tu ? (0-6) : "))
+        if choix < 1 or choix > 7:
+            print(" Colonne incorrect, rappel entre 0-6 !")
+            continue
+
+        if not jouer_colonne(choix):
+            print(" Ah deja pris :( , choisis-en une autre !")
+            continue
+
+        afficher_grille()
+        changer_joueur()
+
+    except ValueError:
+        print("Uniquement un chiffre !")
+
+# si un des joeur rempli les regles pour gagner (4 jetons alignes horizontalement, verticalement ou den diagonale) il est declare gagnant
