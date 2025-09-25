398a36cd7794244589292b226d15e5531930e8c9
Villamayor <villamayorgabriel12@example.com>
2025-09-25 13:54:49 +0200

    Ajout des etapes

---
+++ P4.py
@@ -0,0 +1,9 @@
+# Jeu puissance 4 
+
+# on creer la grille de 6 ranges et 7 colonnes
+
+# jouer 1 = jeton 1 et joueur 2 = jeton 2 
+
+# demander au joueur 1 de choisir la case de la premiere range puis de meme au joueur 2 etc...
+
+# si un des joeur rempli les regles pour gagner (4 jetons alignes horizontalement, verticalement ou den diagonale) il est declare gagnant
