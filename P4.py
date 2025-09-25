62c75135252db5603c9380a5d0c1aaa72d135cad
Villamayor <villamayorgabriel12@example.com>
2025-09-25 23:22:16 +0200

    tableau p4

--- P4.py
+++ P4.py
@@ -2,6 +2,15 @@
 
 # on creer la grille de 6 ranges et 7 colonnes
 
+const = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+]
+
 # jouer 1 = jeton 1 et joueur 2 = jeton 2 
 
 # demander au joueur 1 de choisir la case de la premiere range puis de meme au joueur 2 etc...
